e7367d59463f7fea03ee37d4aed6eb93246cff6e
agent47daun <agent47daun@gmail.com>
2022-05-12 02:12:05 +0400

    Схемы для ответов

--- models/scheme.py
+++ models/scheme.py
@@ -1,10 +1,39 @@
+from datetime import datetime, date
+from typing import List
 from pydantic import BaseModel
 
 
-class UserOutputScheme(BaseModel):
+class BaseScheme(BaseModel):
+
+    class Config:
+        orm_mode = True
+
+
+class UserOutputScheme(BaseScheme):
     id: int
     first_name: str
     last_name: str
 
-    class Config:
-        orm_mode = True
+
+class ShopScheme(BaseScheme):
+    name: str
+    address: str
+
+
+class BookScheme(BaseScheme):
+    name: str
+    author: str
+    release_date: date
+
+
+class OrderItemScheme(BaseScheme):
+    quantity: int
+    book: BookScheme
+    shop: ShopScheme
+    pass
+
+
+class OrderOutputScheme(BaseScheme):
+    items: List[OrderItemScheme]
+    reg_date: datetime
+    user_id: int
