b3cd0557ab8f28e7286b1f1c3c13f8ad768c60fe
agent47daun <agent47daun@gmail.com>
2022-05-11 19:42:29 +0400

    Подготовка для роутов

--- db/base.py
+++ db/base.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+
 DATABASE_URL = "sqlite+aiosqlite:///./project_db.db"
 
-engine = create_async_engine(DATABASE_URL, future=True, echo=True)
-AsyncLocalSession = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+engine = create_async_engine(DATABASE_URL, echo=True)
+LocalAsyncSession = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
-@app.get("/")
-async def hello_world():
-    return {"message": "Hello, World"}
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=1111, host='127.0.0.1', reload=True)
