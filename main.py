b3d172741a5b1e21306364e1a0742b4127ac4484
agent47daun <agent47daun@gmail.com>
2022-05-12 02:13:31 +0400

    Эндпоинт для получения истории Order у User

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 import uvicorn
 from fastapi import FastAPI, Depends
 from fastapi.exceptions import HTTPException
 
 from deps.session import get_session
+from models.models import Shop
 from services.user import get_user_by_id
-from models.scheme import UserOutputScheme
+from services.order import get_user_orders
+from models.scheme import OrderOutputScheme, UserOutputScheme
 
 app = FastAPI()
 
@@ -21,6 +24,12 @@ async def get_user_detail(user_id: int, session: AsyncSession = Depends(get_sess
     if not user:
         raise NotFound()
     return user
+
+
+@app.get("/user/{user_id}/orders/", response_model=List[OrderOutputScheme])
+async def get_user_order_history(user_id: int, session: AsyncSession = Depends(get_session)):
+    orders = await get_user_orders(session, user_id)
+    return orders
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=1111, host='127.0.0.1', reload=True)
