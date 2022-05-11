da63dd111279c76fd9d41eee3a0f400be52552a2
agent47daun <agent47daun@gmail.com>
2022-05-12 02:12:32 +0400

    Where для поиска по конкретному User

--- services/order.py
+++ services/order.py
@@ -1,12 +1,12 @@
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.models import Order, OrderItem
+from models.models import Order, OrderItem, User
 
 
 async def get_user_orders(session: AsyncSession, user_id: int):
-    q = select(Order).options(joinedload(Order.items).joinedload(OrderItem.shop)).options(joinedload(Order.items).joinedload(OrderItem.book))
+    q = select(Order).options(joinedload(Order.items).joinedload(OrderItem.shop)).options(joinedload(Order.items).joinedload(OrderItem.book)).where(User.id == user_id)
     result = await session.execute(q)
     result.unique()
     return result.scalars().all()
