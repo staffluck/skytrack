9d008e64f898348f44b734b8cca8d3b093922fd9
agent47daun <agent47daun@gmail.com>
2022-05-12 15:20:45 +0400

    Pragma foreign_keys при подключении к бд

--- db/base.py
+++ db/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import event
 
 
 DATABASE_URL = "sqlite+aiosqlite:///./project_db.db"
@@ -9,3 +10,12 @@ LocalAsyncSession = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 Base = declarative_base()
+
+
+#  В sqlite3 по дефолту не работает ForeignKeyConstaint, нужно отдельно включать
+def _fk_pragma_on_connect(dbapi_con, con_record):
+    dbapi_con.execute('pragma foreign_keys=ON')
+
+
+# asyncengine пока что не поддерживает event
+event.listen(engine.sync_engine, 'connect', _fk_pragma_on_connect)
