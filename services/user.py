5c99bff6d23320df3e6f2f3f65c3940262ee2426
Arseny <40634016+staffluck@users.noreply.github.com>
2022-05-13 08:46:16 +0400

    Правильный нейминг аргументов

--- services/user.py
+++ services/user.py
@@ -6,13 +6,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from models.models import User
 
 
-async def get_user_by_id(session: AsyncSession, id: int) -> Union[User, None]:
-    q = select(User).where(User.id == id)
+async def get_user_by_id(session: AsyncSession, user_id: int) -> Union[User, None]:
+    q = select(User).where(User.id == user_id)
     result = await session.execute(q)
     return result.scalars().first()
 
 
-async def user_is_exists(session: AsyncSession, id: int) -> bool:
-    q = select(exists(User)).where(User.id == id)
+async def user_is_exists(session: AsyncSession, user_id: int) -> bool:
+    q = select(exists(User)).where(User.id == user_id)
     result = await session.execute(q)
     return result.scalars()
